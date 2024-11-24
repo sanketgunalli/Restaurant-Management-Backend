a022a285fee00aecbca663583abf0b18733f52e3
Sanket <118060438+sanketgunalli@users.noreply.github.com>
2024-11-24 07:59:32 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+//function mongodb database connection
+const connectDb = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log(
+            'Connected to database ${mongoose.connection.host}'.bgCyan
+        );
+    }catch(error){
+        console.log('DB error', error)
+    }
+};
+
+module.exports = connectDb;
+
+
